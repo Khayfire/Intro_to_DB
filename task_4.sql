19db12b07dcbb0cc02ce0f746186c1acad4bcc8a
khayfire <kayode354@gmail.com>
2025-10-17 13:48:42 +0100

    database

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,5 @@
--- Use the alx_book_store database
 USE alx_book_store;
 
--- Retrieve the full description of the 'books' table without using DESCRIBE or EXPLAIN
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
