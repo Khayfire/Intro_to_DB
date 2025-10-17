48731c2a8d6886c97049b6f4201a8826a34f4826
khayfire <kayode354@gmail.com>
2025-10-17 14:02:52 +0100

    database

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,7 @@
--- Use the alx_book_store database
 USE alx_book_store;
 
--- Insert a single row into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
